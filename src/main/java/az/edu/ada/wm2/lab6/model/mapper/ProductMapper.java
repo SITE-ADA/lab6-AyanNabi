14cb959441d65f5eb469b82fba921b74f0b1d5a9
Ayan Nabizada <ayanmna@code.edu.az>
2026-04-02 11:26:06 +0400

    feat: Add Mappers

--- src/main/java/az/edu/ada/wm2/lab6/model/mapper/ProductMapper.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/mapper/ProductMapper.java
@@ -8,18 +8,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-    @Mapping(target = "categoryNames", source = "categories")
+    @Mapping(target = "categoryNames", expression = "java(mapCategoriesToNames(product.getCategories()))")
     ProductResponseDto toResponseDto(Product product);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "categories", ignore = true)
     Product toEntity(ProductRequestDto dto);
 
-    default List<String> mapCategoriesToNames(List<Category> categories) {
+    default List<String> mapCategoriesToNames(Set<Category> categories) {
         return categories == null ? null :
                 categories.stream()
                         .map(Category::getName)
